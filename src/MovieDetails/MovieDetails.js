ea05dfd4e0de44ea5193e5b79cae7845df805d33
Kayla Gordon <heykaylagordon@gmail.com>
2024-09-17 11:39:58 -0600

    Finish

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,9 +1,20 @@
 import './MovieDetails.css';
 
 function MovieDetails({ movie }) {
+  console.log(movie)
+
+  const getGenres = () => {
+    return movie.genre_ids.map(genre => <p className='genre' >{genre}</p>)
+  }
+
   return (
     <section className='MovieDetails'>
-      <p>{movie.title}</p>
+      <img className='backdrop' src={movie.backdrop_path}/>
+      <div className='movie-details'>
+        <h2>{movie.title}</h2>
+        <div>{getGenres()}</div>
+        <p className='overview'>{movie.overview}</p>
+      </div>
     </section>
   );
 }
