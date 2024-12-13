6e656a7e0138b25758ca2fd08a35573eeefaa2de
Karl Fallenius <smilodon.p@gmail.com>
2024-12-12 23:54:49 -0700

    css: fancified the h1 a wee bit

--- src/App/App.js
+++ src/App/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <main className='App'>
         <header>
-          <h1>rancid tomatillos</h1>
+          <h1>rancid tomatillos...</h1>
         </header>
         <section>
           <MainPage mainPage={MainPage} />
