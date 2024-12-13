fbad57533382dddbac0ae6c64c97d2b5921e36db
Karl Fallenius <smilodon.p@gmail.com>
2024-12-12 23:29:16 -0700

    feat: adds buttons but not functionality

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,13 +1,35 @@
 import './MovieCard.css';
+import upvoteIcon from '../icons/upvote.png';
+import downvoteIcon from '../icons/downvote.png';
 
 function MoviePoster({id,posterPath,votes,title}) {
   console.log("poster path: ", {posterPath})
   return (
-    <section className='movie_card'>
+    <div className='movie_card'>
       <img src={posterPath} alt={title}></img>
-      <p className='votes'>{votes}</p>
-    </section>
+      <section className='voting'>
+        <button className='upvote' aria-label='Upvote'>
+          <img src={upvoteIcon}
+           alt="upvote"
+           onClick={() => console.log('Upvoted!')}/>
+        </button>
+        <p className='votes' >{votes}</p>
+        <button className='downvote' aria-label='Downvote'>
+          <img src={downvoteIcon} 
+          alt="downvote"
+          onClick={() => console.log('Downvoted!')}/>
+        </button>
+
+      </section>
+    </div>
   );
 }
 
+function nil() {
+  console.log("nothing")
+}
+
+function alsoNil() {
+  console.log("also nothing")
+}
 export default MoviePoster;
