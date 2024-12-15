7077f67a304affea660cea3eada8d5c43b08e371
Wally Wallace <jrvw001@gmail.com>
2024-12-15 15:08:00 -0700

    Add background image

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,8 +1,11 @@
 import './MovieDetails.css';
 import movieDetails from '../data/movie_details';
 import homeIcon from '../icons/home.png';
+import { useState } from 'react';
 
 function MovieDetails(props) {
+  const [movieDetailsInfo, setMovieDetails] = useState(movieDetails)
+
   return (
     <section className='MovieDetails'>
         <button className='home' aria-label='home'>
@@ -10,6 +13,7 @@ function MovieDetails(props) {
             alt="home"
             onClick={() => props.setMovieDetailsID(null)}/>
         </button>
+        <img src = {movieDetails.backdrop_path} alt = {movieDetails.title}/>
       <p>Movie Details go here!</p>
     </section>
   );
