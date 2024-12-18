89b6b1a58da1c887c2d0aca5dcaf7870c2e73e26
Wally Wallace <jrvw001@gmail.com>
2024-12-18 16:04:16 -0700

    Add test to verify home button is not present

--- cypress/e2e/main.cy.js
+++ cypress/e2e/main.cy.js
@@ -6,8 +6,8 @@ describe('Main Page', () => {
   it('displays title on page load', () => {
     // hint: you'll want to add an intercept here if you are making a network request on page load!
     cy.visit('http://localhost:3000/')
-    .get('h1')
-    .contains('rancid tomatillos')
+    .get('h1').contains('rancid tomatillos')
+    .get('.home-button').should('not.exist')
     .get('.movie-container').find('.movie_card').should('have.lengthOf', 4)
     .get('.movie-container > :nth-child(1)').find('.poster').should('have.attr', 'src', 'https://image.tmdb.org/t/p/original//qJ2tW6WMUDux911r6m7haRef0WH.jpg')
     .get('.movie-container > :nth-child(1)').find('.poster').should('have.attr', 'alt', 'The Dark Knight')
